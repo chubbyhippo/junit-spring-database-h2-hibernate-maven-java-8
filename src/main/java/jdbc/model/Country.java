829f95a00010ea4c97acdd1dd9a98dbee2993613
ch <mkmsacc@outlook.com>
2020-11-25 19:01:01 +0700

    Add mapping tables

--- src/main/java/jdbc/model/Country.java
+++ src/main/java/jdbc/model/Country.java
@@ -1,7 +1,24 @@
 package jdbc.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "COUNTRY")
 public class Country {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	@Column(name = "ID")
+	private int id;
+	
+	@Column(name = "NAME")
 	private String name;
+	
+	@Column(name = "CODE_NAME")
 	private String codeName;
 
 	public Country(String name, String codeName) {
